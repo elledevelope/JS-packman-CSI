51163c073aa2993bbdfcdff44a687ba3937e0efe
elmira <elledevelope@gmail.com>
2024-10-08 16:48:27 +0200

    Limited movement of pacman right and down

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,22 @@
 let pacman = document.getElementById("pacman");
 // console.dir(pacman);
-
 let posX = 0, posY = 0; // Corrected initialization
-console.log(posX, posY);
+// console.log(posX, posY);
+let playground = document.getElementById("playground");
+// console.dir(playground);
 
-//WINDOW IN JS REPRESENTS "la navigateur"
-window.addEventListener("keyup", function (e) { // e - means "event"
+
+let widthPlayground = playground.clientWidth;
+let heightPlayground = playground.clientWidth;
+
+window.addEventListener("keyup", function (e) { // e - means "event", 'window' represents "la navigateur"
     switch (e.keyCode) {
         case 39: //droite
-        case 68: //D            
-            posX += 50; // posX = posX + 50;
+        case 68: //D   
+            if (posX < widthPlayground - 110) {
+                posX += 50; // posX = posX + 50;
+            }
+
             pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
             break;
         case 37: //gauche
@@ -31,7 +38,10 @@ window.addEventListener("keyup", function (e) { // e - means "event"
         case 40: //bas
         case 83: //S
             // posY = posY + 50;
-            posY += 50;
+            // posY += 50;
+            if (posY < heightPlayground - 500) {
+                posY += 50;
+            }
             pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
             break;
 
