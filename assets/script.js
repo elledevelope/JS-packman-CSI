cd131ab3628b45d95808fcfff055e017f4522626
elmira <elledevelope@gmail.com>
2024-10-08 16:08:50 +0200

    Created moving pacman

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+let pacman = document.getElementById("pacman");
+// console.dir(pacman);
+
+let posX = 0, posY = 0; // Corrected initialization
+console.log(posX, posY);
+
+//WINDOW IN JS REPRESENTS "la navigateur"
+window.addEventListener("keyup", function (e) { // e - means "event"
+    switch (e.keyCode) {
+        case 39: //droite
+        case 68: //D
+            // posX = posX + 50;
+            posX +=50;
+            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
+            break;
+        case 37: //gauche
+        case 81:
+            // posX = posX - 50;
+            posX -= 50;
+            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
+            break;
+
+        case 38: //haut
+        case 90: //Z
+            posY -= 50;
+            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
+            break;
+
+        case 40: //bas
+        case 83: //S
+            // posY = posY + 50;
+            posY += 50;
+            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
+            break;
+
+        default:
+            console.log(e.keyCode);
+            break;
+    }
+});
