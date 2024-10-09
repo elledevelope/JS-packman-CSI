76d84b087726d915e70cf100c620023b5ea4b64a
elmira <elledevelope@gmail.com>
2024-10-09 12:07:28 +0200

    Created function movePacman()

--- assets/script.js
+++ assets/script.js
@@ -4,45 +4,62 @@ let posX = 0, posY = 0; // Corrected initialization
 // console.log(posX, posY);
 let playground = document.getElementById("playground");
 // console.dir(playground);
-
-
 let widthPlayground = playground.clientWidth;
 let heightPlayground = playground.clientWidth;
 
-window.addEventListener("keyup", function (e) { // e - means "event", 'window' represents "la navigateur"
-    switch (e.keyCode) {
-        case 39: //droite
-        case 68: //D   
-            if (posX < widthPlayground - 110) {
-                posX += 50; // posX = posX + 50;
-            }
-
-            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
-            break;
-        case 37: //gauche
-        case 81:
+//////////////////////////// -------------------------------------------------- 2nd solution :
+//-------------------------- declaration de fonction
+function movePacman(direction) {
+    //condition qui definit les limites de deplacement de mon pacman :
+    switch (direction) {
+        case "gauche":
             if (posX > 0) {
-                posX -= 50;   // posX = posX - 50;
+                posX -= 50; // posX = posX - 50;
             }
-            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
             break;
-
-        case 38: //haut
-        case 90: //Z
+        case "haut":
             if (posY > 0) {
                 posY -= 50;
             }
-            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
             break;
-
-        case 40: //bas
-        case 83: //S
-            // posY = posY + 50;
-            // posY += 50;
-            if (posY < heightPlayground - 500) {
+        case "droite":
+            if (posX < widthPlayground - 140) {
+                posX += 50;
+            }
+            break;
+        case "bas":
+            if (posY < heightPlayground - 600) {
                 posY += 50;
             }
-            pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
+            break;
+
+        default:
+            break;
+    }
+    pacman.style.transform = "translate(" + posX + "px, " + posY + "px)";
+}
+
+
+//////////////////////////// -------------------------------------------------- 
+window.addEventListener("keyup", function (e) { // e - means "event", 'window' represents "la navigateur"
+    switch (e.keyCode) {
+        case 39: //---------------- droite
+        case 68: //D   
+            movePacman("droite");
+            break;
+        case 37: ////---------------- gauche
+        case 81:
+            movePacman("gauche");
+            break;
+
+        case 38: ////---------------- haut
+        case 90: //Z
+            movePacman("haut");
+            break;
+
+        case 40: // //----------------bas
+        case 83: //S
+            movePacman("bas");
             break;
 
         default:
